4b4e381286a2b428f11fd312e8c403874405ee94
ahmedosama <ahmed.osama.aziz@outlook.com>
2025-01-27 23:08:20 +0200

    Adjust Under Construction Time

--- lib/data/under_construction_data.dart
+++ lib/data/under_construction_data.dart
@@ -1,12 +1,12 @@
 class UnderConstructionData {
-  DateTime targetDate = DateTime(2025, 5, 1);
+  DateTime targetDate = DateTime(2025, 5, 1, 12, 30, 30);
 
   // Getter for today's date to ensure it's always up to date
   DateTime get today => DateTime.now();
 
   // Calculate differences dynamically
   int get diffDays => targetDate.difference(today).inDays;
-  int get diffHours => targetDate.difference(today).inHours;
-  int get diffMins => targetDate.difference(today).inMinutes;
-  int get diffSecs => targetDate.difference(today).inSeconds;
+  int get diffHours => targetDate.difference(today).inHours % 24;
+  int get diffMins => targetDate.difference(today).inMinutes % 60;
+  int get diffSecs => targetDate.difference(today).inSeconds % 60;
 }
