5861553b4cbcb297e86ccf6f474cd955d34a61c6
ahmedabdomadkour@gmail.com <ahmedabdomadkour@gmail.com>
2025-03-20 12:59:55 +0200

    create part from careers modela and cubit

--- lib/pages/careers_page/sections/benefits_perks_section/layout/mobile.dart
+++ lib/pages/careers_page/sections/benefits_perks_section/layout/mobile.dart
@@ -1,3 +1,4 @@
+import 'package:devloopy_website/data/ui_data/careers_ui_data/section_details/benfits_ui_data.dart';
 import 'package:devloopy_website/pages/careers_page/sections/benefits_perks_section/widget/layout/mobile.dart';
 import 'package:devloopy_website/widgets/custom_second_dsecription/layout/mobile.dart';
 import 'package:devloopy_website/widgets/custom_title_section/layout/mobile.dart';
@@ -8,8 +9,8 @@ class BenefitsPerksSectionMobile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.only(
+    return Padding(
+      padding: const EdgeInsets.only(
         top: 50,
         left: 16,
         right: 16,
@@ -17,14 +18,13 @@ class BenefitsPerksSectionMobile extends StatelessWidget {
       child: Column(
         children: [
           CustomTitelSectionMobile(
-            title: "Benefitss",
-            subTitle: " & Perk",
+            title: benfitsUiData.title,
+            subTitle: benfitsUiData.subTitle,
           ),
           CustomSecondDescriptionMobile(
-            description:
-                "At DevLoopy, we believe in prioritizing the well-being and growth of our team members. We offer a comprehensive range of benefits and perks to ensure our employees feel valued, motivated, and supported in both their personal and professional lives. As a member of the DevLoopy family, you can enjoy the following benefits",
+            description: benfitsUiData.description,
           ),
-          CardBenefitsAndPerksMobile(),
+          const CardBenefitsAndPerksMobile(),
         ],
       ),
     );
--- lib/pages/careers_page/sections/employee_referral_program_section/layout/desktop.dart
+++ lib/pages/careers_page/sections/employee_referral_program_section/layout/desktop.dart
@@ -1,3 +1,4 @@
+import 'package:devloopy_website/data/ui_data/careers_ui_data/section_details/employee_referal_ui_data.dart';
 import 'package:devloopy_website/pages/careers_page/sections/employee_referral_program_section/card_employee_referral/layout/desktop.dart';
 import 'package:devloopy_website/widgets/custom_description_section/layout/desktop.dart';
 import 'package:devloopy_website/widgets/custom_title_section/layout/desktop.dart';
@@ -8,8 +9,8 @@ class EmployeeReferralProgramSectionDeskTop extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.only(
+    return Padding(
+      padding: const EdgeInsets.only(
         top: 80,
         left: 150,
         right: 150,
@@ -17,16 +18,15 @@ class EmployeeReferralProgramSectionDeskTop extends StatelessWidget {
       child: Column(
         children: [
           CustomTitelSectionDeskTop(
-            title: "Referral ",
-            subTitle: "Program",
+            title: employeeReferalUiData.title,
+            subTitle: employeeReferalUiData.subTitle,
           ),
-          SizedBox(height: 20),
+          const SizedBox(height: 20),
           CustomDescriptionSectionDesktop(
-            descriptionSection:
-                "At DevLoopy, we believe that our employees are our most valuable asset. We recognize the importance of building a strong and talented team, and we encourage our employees to play an active role in shaping our workforce. Introducing our Employee Referral Program – an opportunity for you to help us find exceptional talent and be rewarded for your contributions.",
+            descriptionSection: employeeReferalUiData.description,
           ),
-          SizedBox(height: 20),
-          CardEmployeeReferralDeskTop(),
+          const SizedBox(height: 20),
+          const CardEmployeeReferralDeskTop(),
         ],
       ),
     );
